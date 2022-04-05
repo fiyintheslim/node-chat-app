bd8aaa1349f7d9164b310d38d12fba06b6be60d4
fiyintheslim <fiyinoyekunle@gmail.com>
2022-04-05 13:48:13 +0100

    database

--- backend/typescript/config/postgresSetup.ts
+++ backend/typescript/config/postgresSetup.ts
@@ -8,12 +8,10 @@ const dbConnection = async ()=>{
     
     if(process.env.NODE_ENV === "production"){
          let pool =await new Pool({
-            user : process.env.DB_USER,
-            host : process.env.DB_HOST,
-            password : process.env.DB_PASSWORD,
-            port : process.env.DB_PORT,
-            database : process.env.DB,
-            ssl:true
+            connectionString: process.env.DATABASE_URL,
+            ssl: {
+                rejectUnauthorized: false,
+            },
         })
         return pool
     }
